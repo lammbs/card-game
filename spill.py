8a4e8fa91fb8c5aca162352f37592f727cbf79d2
lammbs <24aurjen01@gmail.com>
2026-03-26 14:31:11 +0100

    Update spill.py
    m

---
+++ spill.py
@@ -0,0 +1,16 @@
+import random
+
+class Cards:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+    
+    def hand(n):
+        print("")
+        
+
+suits = ["\u2663", "\u2665", "\u2666", "\u2660"]
+ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
+deck = [Cards(rank, suit) for suit in suits for rank in ranks]
+
