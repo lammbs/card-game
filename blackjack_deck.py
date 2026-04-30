c69e83367567bcb4f5bd16eb72f5b99395129266
lammbs <24aurjen01@gmail.com>
2026-04-30 10:24:14 +0200

    button click yay
    i clickd da buton

--- blackjack_deck.py
+++ blackjack_deck.py
@@ -3,32 +3,20 @@ import pygame
 import tkinter
 
 
-# the game window
-window = tkinter.Tk()
-window.title("Jackblack")
-
-# the game canvas
-canvas = tkinter.Canvas(window, bg = "green", borderwidth = 0, highlightthickness =0)
-canvas.pack()
-
-window.update()
-
-# centering the game window
-window_width = window.winfo_width()
-window_height = window.winfo_height()
-screen_width = window.winfo_screenwidth()
-screen_height = window.winfo_screenheight()
-
-
 class Cards:
     suits = ["\u2663", "\u2665", "\u2666", "\u2660"]
     ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
-    def __init__(self, suit, rank):
-        self.suit = suit
-        self.rank = rank
+    def __init__(self):
+        self.deck = [{'suit': suit, 'rank': rank} for suit in self.suits for rank in self.ranks]
     
-
-
-window.focus_set()
-window.mainloop()
+    def shuffle(self):
+        random.shuffle(self.deck)
+    
+    def deal(self):
+        if len(self.deck) < 2:
+            raise ValueError("Not enough cards to deal")
+        card1 = self.deck.pop()
+        card2 = self.deck.pop()
+        return card1, card2
+        
--- blackjack_game.py
+++ blackjack_game.py
@@ -1,19 +1,46 @@
 import random
 import pygame
+import blackjack_deck
+import blackjack_player
+import blackjack_buttons
 
+# The game window
 pygame.init()
-window = pygame.display.set_mode((1000,500), pygame.RESIZABLE)
+window = pygame.display.set_mode((1920,1200), pygame.RESIZABLE)
 pygame.display.set_caption("Jackass")
 
+# Images
+title = pygame.image.load("img/Skjermbilde_2026-04-30_091201-removebg-preview.png").convert_alpha()
+playbutton = pygame.image.load("img/play-button-icon-png-5-removebg-preview.png").convert_alpha()
+rules = pygame.image.load("img/466-4660322_rules-test-rules-icon-transparent-background-removebg-preview.png").convert_alpha()
+exitbutton = pygame.image.load("img/3E0657-1PTE-removebg-preview.png").convert_alpha()
 
 
+playbutton = blackjack_buttons.Buttons(500, 270, playbutton, 1.8)
+rules = blackjack_buttons.Buttons(-10, 1100, rules, 0.222)
+exitbutton = blackjack_buttons.Buttons(1820, 1100, exitbutton, 0.199)
+
+# Game loop
 run = True
 while run:
     pygame.time.delay(60)
 
+    # Background color
     window.fill((43, 130, 57))
-    
+
+    # Display images
+    window.blit(title, (400, 50))
+
+    if playbutton.draw(window):
+        print("Play button clicked")
+    rules.draw(window)
+    if exitbutton.draw(window):
+        run = False
+
+
+    # Event handler
     for event in pygame.event.get():
+        # Quit game
         if event.type == pygame.QUIT:
             run = False
 
